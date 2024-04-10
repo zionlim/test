4330ebf562974208b8324608a89988f17015c701
zionlim <139849003+zionlim@users.noreply.github.com>
2024-04-10 15:45:31 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include "./inc/header.php"; 
+	  include "./inc/setup_session.php";
+
+?>
+    <main>
+        <section class="banner">
+            <h1>Your culinary adventure awaits</h1>
+            <button>Order Now</button>
+        </section>
+        <section class="weekly-specials">
+            <h1>Our weekly specials</h1>
+            <div class="item-container">
+                <div class="item">
+                    <img src="./src/img/fooditems/food1.png" alt="">
+                    <div class="item-des">
+                        <p class="item-name">Item 1</p>
+                        <p class="item-subtext">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="./src/img/fooditems/food2.png" alt="">
+                    <div class="item-des">
+                        <p class="item-name">Item 2</p>
+                        <p class="item-subtext">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="./src/img/fooditems/food3.png" alt="">
+                    <div class="item-des">
+                        <p class="item-name">Item 3</p>
+                        <p class="item-subtext">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+        Project for IE4717 by Zaw and Zion
+    </footer>
+</body>
+
+</html>
